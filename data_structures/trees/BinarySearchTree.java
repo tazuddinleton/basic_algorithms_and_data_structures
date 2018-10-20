2389919befc8d7ba278a00ac47235066b444d233
Taz Uddin <getleton@gmail.com>
2018-10-20 22:05:12 +0600

    binary search tree implemented

---
+++ data_structures/trees/BinarySearchTree.java
@@ -0,0 +1,56 @@
+/**
+ * 1. insert 2. find 3. traversal a. ordered traversal b. inordered traversal.
+ * 
+ * Ordered traversal : root -> left -> right. InOrdred traversal : left -> root.
+ * -> right.
+ */
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 1, 5, 5, 3, 3, 3, 4 };
+
+        Node tree = new Node(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            tree.insert(arr[i]);
+        }
+
+        tree.printInOrder();
+
+    }
+}
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int value) {
+        this.data = value;
+    }
+
+    public void insert(int value) {
+
+        if (value <= data) {
+            if (left != null) {
+                left.insert(value);
+            } else {
+                left = new Node(value);
+            }
+        } else {
+            if (right != null) {
+                right.insert(value);
+            } else {
+                right = new Node(value);
+            }
+        }
+    }
+
+    public void printInOrder() {
+        if (left != null) {
+            left.printInOrder();
+        }
+        System.out.println(data);
+        if (right != null) {
+            right.printInOrder();
+        }
+    }
+}
