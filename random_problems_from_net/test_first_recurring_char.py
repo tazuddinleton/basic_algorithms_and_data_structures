c0ed48a32f46f516a55ed6a221d07b2cb0c45ed2
Taz Uddin <getleton@gmail.com>
2019-02-24 21:52:39 +0600

    clean up

--- random_problems_from_net/test_first_recurring_char.py
+++ random_problems_from_net/test_first_recurring_char.py
@@ -4,15 +4,7 @@ from first_recurring_char import *
 
 class TestFirstRecurrChar(unittest.TestCase):
     def test_first_recurr_char(self):
-        # str = 'ABCDA'
-        test1 = 'ABCDA'
-        self.assertEquals(first_recurr_char(test1), 'A')
-        # str = 'AAAAAAAAAAA'
-        test2 = 'AAAAAAAAAAA'
-        self.assertEquals(first_recurr_char(test2), 'A')
-        # str = 'ABCDEFGHIJKLMNOPQRSTUVWXX'
-        test3 = 'ABCDEFGHIJKLMNOPQRSTUVWXX'
-        self.assertEquals(first_recurr_char(test3), 'X')
-        # str = 'ABCDEFGHIJKLMNOPQRSTUVWX'
-        test4 = 'ABCDEFGHIJKLMNOPQRSTUVWX'
-        self.assertEquals(first_recurr_char(test4), None)
+        self.assertEquals(first_recurr_char('ABCDA'), 'A')
+        self.assertEquals(first_recurr_char('AAAAAAAAAAA'), 'A')
+        self.assertEquals(first_recurr_char('ABCDEFGHIJKLMNOPQRSTUVWXX'), 'X')
+        self.assertEquals(first_recurr_char('ABCDEFGHIJKLMNOPQRSTUVWX'), None)
