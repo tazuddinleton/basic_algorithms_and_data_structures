f197d02be7c5f74d930abfc6c311bbcfc39bd71d
Taz Uddin <getleton@gmail.com>
2019-12-01 21:34:42 +0600

    refactor bubble sort

--- hackerrank/interview_preparation_kit/arrays/minimum_bribes.py
+++ hackerrank/interview_preparation_kit/arrays/minimum_bribes.py
@@ -1,14 +1,38 @@
-def minimumBribes(q):    
-    br = 0
-    for i in range(0, len(q)):                
-        if q[i] - (i+1) > 2:
-            print('Too chaotic')
-            return        
-    ## Look back and count for each person how many people have over taken him
-        for j in range(0, i):
-            if q[j] > q[i]:
-                br += 1
-    print(br)
+
+def minimumBribes(a):
+    isSorted = False
+    count = 0
+    end = len(a)-1
+    while not isSorted:
+        isSorted = True        
+        for i in range(0, end):
+            if a[i] - (i+1) > 2:
+                print('Too chaotic')
+                return
+            if a[i] > a[i+1]:
+                isSorted = False
+                count += 1
+                t = a[i]
+                a[i] = a[i+1]
+                a[i+1] = t
+        end -= 1
+    print(count)
+
+
+
+
+
+# def minimumBribes(q):    
+#     br = 0
+#     for i in range(0, len(q)):                
+#         if q[i] - (i+1) > 2:
+#             print('Too chaotic')
+#             return        
+#     ## Look back and count for each person how many people have over taken him
+#         for j in range(0, i):
+#             if q[j] > q[i]:
+#                 br += 1
+#     print(br)
     
 minimumBribes([2,1,5,3,4])
 
